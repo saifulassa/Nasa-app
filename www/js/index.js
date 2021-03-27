e6fd3aab4797a3738038ad0480ec22237b72b4a8
saifulassa <saifulassa@gmail.com>
2021-03-27 22:24:55 +0700

    penambahan API key Nasa

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=hSZSEl3STY1vFypmAlqsvsPcIaOeXyFlb2pbhbmx";
         
         $("#loadImage").click(function(){
             $.ajax({
